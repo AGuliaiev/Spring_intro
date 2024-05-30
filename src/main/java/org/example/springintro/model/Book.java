373004bc35310e37a249964f8d62124546d9e462
Gulini32 <gulini32@gmail.com>
2024-05-30 15:13:16 +0300

    data-jpa

--- src/main/java/org/example/springintro/model/Book.java
+++ src/main/java/org/example/springintro/model/Book.java
@@ -6,6 +6,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import java.math.BigDecimal;
 import lombok.Getter;
 import lombok.Setter;
 import org.hibernate.annotations.SQLDelete;
@@ -21,10 +22,14 @@ public class Book {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(nullable = false)
     private String title;
+    @Column(nullable = false)
     private String author;
+    @Column(nullable = false, unique = true)
     private String isbn;
-    private String price;
+    @Column(nullable = false)
+    private BigDecimal price;
     private String description;
     private String coverImage;
     @Column(nullable = false)
