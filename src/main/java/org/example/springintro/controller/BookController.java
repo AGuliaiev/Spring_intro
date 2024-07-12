1e722db4618cc0d4bfcf36f0e947150dbea91d40
Gulini32 <gulini32@gmail.com>
2024-07-12 14:03:43 +0300

    add_global_exception

--- src/main/java/org/example/springintro/controller/BookController.java
+++ src/main/java/org/example/springintro/controller/BookController.java
@@ -1,5 +1,6 @@
 package org.example.springintro.controller;
 
+import jakarta.validation.Valid;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.example.springintro.dto.BookDto;
@@ -36,7 +37,7 @@ public class BookController {
     }
 
     @PostMapping
-    public BookDto createBook(@RequestBody CreateBookRequestDto requestDto) {
+    public BookDto createBook(@RequestBody @Valid CreateBookRequestDto requestDto) {
         return bookService.save(requestDto);
     }
 
--- src/main/java/org/example/springintro/dto/CreateBookRequestDto.java
+++ src/main/java/org/example/springintro/dto/CreateBookRequestDto.java
@@ -1,6 +1,8 @@
 package org.example.springintro.dto;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 @Data
@@ -9,7 +11,10 @@ public class CreateBookRequestDto {
     private String title;
     private String author;
     private String isbn;
+    @Min(0)
     private String price;
+    @NotNull
     private String description;
+    @NotNull
     private String coverImage;
 }
