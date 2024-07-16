ad464944496f3004af832418fa1dc0e8928bdfcb
Андрей Гуляев <123542367+AGuliaiev@users.noreply.github.com>
2024-07-16 15:34:57 +0300

    Master query (#5)
    * spring repo
    
    * data-jpa
    
    * data-jpa
    
    * data-jpa
    
    * data-jpa
    
    * data-jpa
    
    * data-jpa
    
    * data-jpa
    
    ---------
    
    Co-authored-by: Gulini <gulinoi32@gmail.com>

---
+++ src/main/java/org/example/springintro/repository/book/BookSpecificationBuilder.java
@@ -0,0 +1,29 @@
+package org.example.springintro.repository.book;
+
+import lombok.RequiredArgsConstructor;
+import org.example.springintro.dto.BookSearchParameters;
+import org.example.springintro.model.Book;
+import org.example.springintro.repository.SpecificationBuilder;
+import org.example.springintro.repository.SpecificationProviderManager;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.stereotype.Component;
+
+@RequiredArgsConstructor
+@Component
+public class BookSpecificationBuilder implements SpecificationBuilder<Book> {
+    private final SpecificationProviderManager<Book> bookSpecificationProviderManager;
+
+    @Override
+    public Specification<Book> build(BookSearchParameters searchParameters) {
+        Specification<Book> spec = Specification.where(null);
+        if (searchParameters.title() != null && searchParameters.title().length > 0) {
+            spec = spec.and(bookSpecificationProviderManager.getSpecificationProvider("title")
+                    .getSpecification(searchParameters.title()));
+        }
+        if (searchParameters.author() != null && searchParameters.author().length > 0) {
+            spec = spec.and(bookSpecificationProviderManager.getSpecificationProvider("author")
+                    .getSpecification(searchParameters.author()));
+        }
+        return spec;
+    }
+}
