5772cf6c667f80aab4748ee8d7e02cda890ec1f2
Gulini32 <gulini32@gmail.com>
2024-07-22 13:37:46 +0300

    add validation

--- src/main/java/org/example/springintro/dto/CreateBookRequestDto.java
+++ src/main/java/org/example/springintro/dto/CreateBookRequestDto.java
@@ -2,22 +2,24 @@ package org.example.springintro.dto;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 @Data
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class CreateBookRequestDto {
-    @NotNull
+    @NotBlank
     private String title;
-    @NotNull
+    @NotBlank
     private String author;
-    @NotNull
+    @NotBlank
     private String isbn;
+    @NotNull
     @Min(0)
     private String price;
-    @NotNull
+    @NotBlank
     private String description;
-    @NotNull
+    @NotBlank
     private String coverImage;
 }
