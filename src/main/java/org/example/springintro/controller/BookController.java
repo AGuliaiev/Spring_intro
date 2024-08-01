761cb1df9213d693b0913d6728bf8793db943b58
Андрей Гуляев <123542367+AGuliaiev@users.noreply.github.com>
2024-08-01 09:26:23 +0300

    add_page (#8)
    * spring repo
    
    * spring repo
    
    * spring repo
    
    * spring data jpa
    
    * data-jpa
    
    * data-jpa
    
    * data-jpa
    
    * data-jpa
    
    * data-jpa
    
    * data-jpa
    
    * data-jpa
    
    * data-jpa
    
    * add_global_exception
    
    * add_global_exception
    
    * add_global_exception
    
    * add_page
    
    * add_except
    
    * Revert "Merge remote-tracking branch 'origin/glob_except' into glob_except"
    
    This reverts commit 0950836319cd0b6c77b0964bbd905cab93cb6fbf, reversing
    changes made to 3de5638582b5f60d30e3cb4d3f2ec3b64d9eeeea.
    
    * Revert "Merge remote-tracking branch 'origin/glob_except' into glob_except"
    
    This reverts commit 0950836319cd0b6c77b0964bbd905cab93cb6fbf, reversing
    changes made to 3de5638582b5f60d30e3cb4d3f2ec3b64d9eeeea.
    
    * Revert "Merge remote-tracking branch 'origin/glob_except' into glob_except"
    
    This reverts commit 0950836319cd0b6c77b0964bbd905cab93cb6fbf, reversing
    changes made to 3de5638582b5f60d30e3cb4d3f2ec3b64d9eeeea.
    
    * add validation
    
    * add validation
    
    * add Swagger annotations
    
    * add validation to public BookDto updateBook
    
    * add rename to isDeleted
    
    * add rename to isDeleted
    
    * add rename to isDeleted
    
    ---------
    
    Co-authored-by: Gulini <gulinoi32@gmail.com>

--- src/main/java/org/example/springintro/controller/BookController.java
+++ src/main/java/org/example/springintro/controller/BookController.java
@@ -1,5 +1,7 @@
 package org.example.springintro.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
@@ -8,6 +10,9 @@ import org.example.springintro.dto.BookSearchParameters;
 import org.example.springintro.dto.CreateBookRequestDto;
 import org.example.springintro.mapper.BookMapper;
 import org.example.springintro.services.BookService;
+import org.springdoc.core.annotations.ParameterObject;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -19,6 +24,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(
+        name = "Book shop",
+        description = "Endpoints for Endpoints for managing books in the book shop"
+)
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/books")
@@ -27,22 +36,26 @@ public class BookController {
     private final BookMapper bookMapper;
 
     @GetMapping
-
-    public List<BookDto> getAll() {
-        return bookService.findAll();
+    @Operation(summary = "Get all books", description = "Get all books from the book shop")
+    public List<BookDto> getAll(@ParameterObject @PageableDefault Pageable pageable) {
+        return bookService.findAll(pageable);
     }
 
     @GetMapping("/{id}")
+    @Operation(summary = "Get book by id", description = "Get book by id from the book shop")
     public BookDto getBookById(@PathVariable Long id) {
         return bookService.findById(id);
     }
 
     @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    @Operation(summary = "Create book", description = "Create book in the book shop")
     public BookDto createBook(@RequestBody @Valid CreateBookRequestDto requestDto) {
         return bookService.save(requestDto);
     }
 
     @PutMapping("/{id}")
+    @Operation(summary = "Update book", description = "Update book in the book shop")
     public BookDto updateBook(
             @PathVariable Long id,
             @RequestBody @Valid CreateBookRequestDto requestDto
@@ -52,12 +65,17 @@ public class BookController {
 
     @ResponseStatus(HttpStatus.NO_CONTENT)
     @DeleteMapping("/{id}")
+    @Operation(summary = "Delete book", description = "Delete book from the book shop")
     public void deleteBook(@PathVariable Long id) {
         bookService.deleteById(id);
     }
 
     @GetMapping("/search")
-    public List<BookDto> searchBooks(BookSearchParameters searchParameters) {
-        return bookService.search(searchParameters);
+    @Operation(summary = "Search books", description = "Search books in the book shop")
+    public List<BookDto> searchBooks(
+            @ParameterObject @PageableDefault Pageable pageable,
+            BookSearchParameters searchParameters
+    ) {
+        return bookService.search(searchParameters, pageable);
     }
 }
