b8c8f34608bf2eb1097af34adda5270648f17e74
Gulini32 <gulini32@gmail.com>
2024-08-01 09:25:15 +0300

    add rename to isDeleted

--- src/main/java/org/example/springintro/controller/BookController.java
+++ src/main/java/org/example/springintro/controller/BookController.java
@@ -1,6 +1,5 @@
 package org.example.springintro.controller;
 
-
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
@@ -40,9 +39,6 @@ public class BookController {
     @Operation(summary = "Get all books", description = "Get all books from the book shop")
     public List<BookDto> getAll(@ParameterObject @PageableDefault Pageable pageable) {
         return bookService.findAll(pageable);
-
-    public List<BookDto> getAll() {
-        return bookService.findAll();
     }
 
     @GetMapping("/{id}")
