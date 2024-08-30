24796f6d199361bfdf5cac263e704955370878b2
Андрей Гуляев <123542367+AGuliaiev@users.noreply.github.com>
2024-08-30 10:34:54 +0300

    Spring scurity (#12)
    * spring repo
    
    * spring repo
    
    * spring repo
    
    * spring data jpa
    
    * data-jpa
    
    * data-jpa
    
    * data-jpa
    
    * data-jpa
    
    * data-jpa
    
    * data-jpa
    
    * data-jpa
    
    * data-jpa
    
    * add_global_exception
    
    * add_global_exception
    
    * add_global_exception
    
    * add_page
    
    * add_except
    
    * Revert "Merge remote-tracking branch 'origin/glob_except' into glob_except"
    
    This reverts commit 0950836319cd0b6c77b0964bbd905cab93cb6fbf, reversing
    changes made to 3de5638582b5f60d30e3cb4d3f2ec3b64d9eeeea.
    
    * Revert "Merge remote-tracking branch 'origin/glob_except' into glob_except"
    
    This reverts commit 0950836319cd0b6c77b0964bbd905cab93cb6fbf, reversing
    changes made to 3de5638582b5f60d30e3cb4d3f2ec3b64d9eeeea.
    
    * Revert "Merge remote-tracking branch 'origin/glob_except' into glob_except"
    
    This reverts commit 0950836319cd0b6c77b0964bbd905cab93cb6fbf, reversing
    changes made to 3de5638582b5f60d30e3cb4d3f2ec3b64d9eeeea.
    
    * add validation
    
    * add validation
    
    * add Swagger annotations
    
    * add validation to public BookDto updateBook
    
    * add rename to isDeleted
    
    * add rename to isDeleted
    
    * add rename to isDeleted
    
    * add User entity
    
    * add User entity
    
    * add User entity
    
    * add User entity
    
    * bug fixes
    
    ---------
    
    Co-authored-by: Gulini <gulinoi32@gmail.com>

---
+++ src/main/java/org/example/springintro/security/CustomUserDetailsService.java
@@ -0,0 +1,22 @@
+package org.example.springintro.security;
+
+import lombok.RequiredArgsConstructor;
+import org.example.springintro.exception.EntityNotFoundException;
+import org.example.springintro.repository.user.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email)
+            throws UsernameNotFoundException {
+        return userRepository.findByEmail(email)
+                .orElseThrow(() -> new EntityNotFoundException("Can't find user by email"));
+    }
+}
