aa8eb77138d1477d5ddbf471221802fec367dd96
Gulini32 <gulini32@gmail.com>
2024-09-02 16:43:17 +0300

    add JWT support

--- src/main/java/org/example/springintro/security/JwtAuthenticationFilter.java
+++ src/main/java/org/example/springintro/security/JwtAuthenticationFilter.java
@@ -6,6 +6,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -18,11 +19,10 @@ import org.springframework.web.filter.OncePerRequestFilter;
 @Component
 @RequiredArgsConstructor
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
+    private static final String AUTHORIZATION = "Authorization";
+    private static final String TOKEN_HEADER = "Bearer ";
     private final JwtUtil jwtUtil;
     private final UserDetailsService userDetailsService;
-
-    private final String authorizationHeader = "Authorization";
-    private final String bearerPrefix = "Bearer ";
 
     @Override
     protected void doFilterInternal(
@@ -45,9 +45,9 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
     }
 
     private String getToken(HttpServletRequest request) {
-        String bearerToken = request.getHeader(authorizationHeader);
-        if (StringUtils.hasText(bearerToken) && bearerToken.startsWith(bearerPrefix)) {
-            return bearerToken.substring(bearerPrefix.length());
+        String bearerToken = request.getHeader(HttpHeaders.AUTHORIZATION);
+        if (StringUtils.hasText(bearerToken) && bearerToken.startsWith(TOKEN_HEADER)) {
+            return bearerToken.substring(TOKEN_HEADER.length());
         }
         return null;
     }
