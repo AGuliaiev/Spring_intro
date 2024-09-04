3769857cb4c2908d1101d2a73ee7f49229c098a1
Андрей Гуляев <123542367+AGuliaiev@users.noreply.github.com>
2024-09-04 10:19:39 +0300

    Jwt sicret (#13)
    * spring repo
    
    * spring repo
    
    * spring repo
    
    * spring data jpa
    
    * data-jpa
    
    * data-jpa
    
    * data-jpa
    
    * data-jpa
    
    * data-jpa
    
    * data-jpa
    
    * data-jpa
    
    * data-jpa
    
    * add_global_exception
    
    * add_global_exception
    
    * add_global_exception
    
    * add_page
    
    * add_except
    
    * Revert "Merge remote-tracking branch 'origin/glob_except' into glob_except"
    
    This reverts commit 0950836319cd0b6c77b0964bbd905cab93cb6fbf, reversing
    changes made to 3de5638582b5f60d30e3cb4d3f2ec3b64d9eeeea.
    
    * Revert "Merge remote-tracking branch 'origin/glob_except' into glob_except"
    
    This reverts commit 0950836319cd0b6c77b0964bbd905cab93cb6fbf, reversing
    changes made to 3de5638582b5f60d30e3cb4d3f2ec3b64d9eeeea.
    
    * Revert "Merge remote-tracking branch 'origin/glob_except' into glob_except"
    
    This reverts commit 0950836319cd0b6c77b0964bbd905cab93cb6fbf, reversing
    changes made to 3de5638582b5f60d30e3cb4d3f2ec3b64d9eeeea.
    
    * add validation
    
    * add validation
    
    * add Swagger annotations
    
    * add validation to public BookDto updateBook
    
    * add rename to isDeleted
    
    * add rename to isDeleted
    
    * add rename to isDeleted
    
    * add User entity
    
    * add User entity
    
    * add User entity
    
    * add User entity
    
    * add User entity
    
    * bug fixes
    
    * add JWT support
    
    * add JWT support
    
    * add JWT support
    
    * add JWT support
    
    ---------
    
    Co-authored-by: Gulini <gulinoi32@gmail.com>

---
+++ src/main/java/org/example/springintro/dto/user/UserLoginResponseDto.java
@@ -0,0 +1,4 @@
+package org.example.springintro.dto.user;
+
+public record UserLoginResponseDto(String token) {
+}
--- src/main/java/org/example/springintro/services/impl/BookServiceImpl.java
+++ src/main/java/org/example/springintro/services/impl/BookServiceImpl.java
@@ -29,7 +29,6 @@ public class BookServiceImpl implements BookService {
     }
 
     @Override
-
     public List<BookDto> findAll(Pageable pageable) {
         return bookRepository.findAll(pageable).stream()
                 .map(bookMapper::toDto)
