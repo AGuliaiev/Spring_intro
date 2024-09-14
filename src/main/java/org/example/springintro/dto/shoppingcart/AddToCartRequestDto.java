d5a5689de2c90d1d14b61d32a860d34e441de66d
Gulini32 <gulini32@gmail.com>
2024-09-14 10:27:16 +0300

    add Sopping Cart

--- src/main/java/org/example/springintro/dto/shoppingcart/AddToCartRequestDto.java
+++ src/main/java/org/example/springintro/dto/shoppingcart/AddToCartRequestDto.java
@@ -1,11 +1,13 @@
 package org.example.springintro.dto.shoppingcart;
 
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 import lombok.Data;
 
 @Data
 public class AddToCartRequestDto {
     @Positive
+    @NotNull
     private Long bookId;
     @Positive
     private int quantity;
